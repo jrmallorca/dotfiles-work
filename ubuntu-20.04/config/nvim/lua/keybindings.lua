a14151956dd72488d904a5ab3de878fac6568a16
Jonquil Isys Mallorca <48072014+jrmallorca@users.noreply.github.com>
2022-03-14 10:52:06 +0000

    Corrected nvim keybindings

--- ubuntu-20.04/config/nvim/lua/keybindings.lua
+++ ubuntu-20.04/config/nvim/lua/keybindings.lua
@@ -1,49 +1,47 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
 -- Remap space as leader key
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+map('', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- \w = Save current file
 -- \q = Quit
 -- \x = Write all buffers and quit
-vim.api.nvim_set_keymap('n', '<Bslash>w', [[<Cmd>w<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Bslash>q', [[<Cmd>q<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Bslash>x', [[<Cmd>wqa<CR>]], { noremap = true, silent = true })
+map('n', '<Bslash>w', [[<Cmd>w<CR>]], opts)
+map('n', '<Bslash>q', [[<Cmd>q<CR>]], opts)
+map('n', '<Bslash>x', [[<Cmd>wqa<CR>]], opts)
 
 -- TrueZen --
 -- <Leader>1 = Ataraxis mode
 -- <Leader>2 = Minimalist mode
 -- <Leader>3 = Focus mode
 -- <Leader>4 = Turn off TrueZen
-vim.api.nvim_set_keymap('n', '<Leader>1', [[<Cmd>TZAtaraxisOff<CR><Cmd>TZMinimalistOff<CR><Cmd>TZFocusOff<CR><Cmd>TZAtaraxisOn<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>2', [[<Cmd>TZAtaraxisOff<CR><Cmd>TZMinimalistOff<CR><Cmd>TZFocusOff<CR><Cmd>TZMinimalistOn<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>3', [[<Cmd>TZFocus<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>4', [[<Cmd>TZAtaraxisOff<CR><Cmd>TZMinimalistOff<CR><Cmd>TZFocusOff<CR>]], { noremap = true, silent = true })
+map('n', '<Leader>1', [[<Cmd>TZAtaraxisOff<CR><Cmd>TZMinimalistOff<CR><Cmd>TZFocusOff<CR><Cmd>TZAtaraxisOn<CR>]], opts)
+map('n', '<Leader>2', [[<Cmd>TZAtaraxisOff<CR><Cmd>TZMinimalistOff<CR><Cmd>TZFocusOff<CR><Cmd>TZMinimalistOn<CR>]], opts)
+map('n', '<Leader>3', [[<Cmd>TZFocus<CR>]], opts)
+map('n', '<Leader>4', [[<Cmd>TZAtaraxisOff<CR><Cmd>TZMinimalistOff<CR><Cmd>TZFocusOff<CR>]], opts)
 
--- Snap --
+-- Fuzzy finder --
 -- \\  = Find files within current and child directories
 -- \fb = Find open files in buffers
--- \fo = Find files previously opened in history
 -- \/  = Find matching string within current and child directories
-local snap = require'snap'
-snap.maps {
-  {"<Bslash><Bslash>", snap.config.file {producer = "ripgrep.file"}},
-  {"<Bslash>fb", snap.config.file {producer = "vim.buffer"}},
-  {"<Bslash>fo", snap.config.file {producer = "vim.oldfile"}},
-  {"<Bslash>/", snap.config.vimgrep {}},
-}
+map('n', '<Bslash><Bslash>', [[<Cmd>Telescope find_files<CR>]], opts)
+map('n', '<Bslash>fb', [[<Cmd>Telescope buffers<CR>]], opts)
+map('n', '<Bslash>/', [[<Cmd>Telescope live_grep<CR>]], opts)
 
 -- Lightspeed
 -- Replace 's' with 'f'
-vim.api.nvim_set_keymap('n', 'f', [[<Plug>Lightspeed_s]], { silent = true })
-vim.api.nvim_set_keymap('n', 'F', [[<Plug>Lightspeed_S]], { silent = true })
+map('n', 'f', [[<Plug>Lightspeed_s]], opts)
+map('n', 'F', [[<Plug>Lightspeed_S]], opts)
 
 -- Ultest
 -- ]t         = Find next failed test, direction up
 -- [t         = Find next failed test, direction down
 -- <Leader>tt = Test everything
 -- <Leader>tn = Test nearest to cursor
-vim.api.nvim_set_keymap('n', ']t', [[<Plug>(ultest-next-fail)]], { silent = true })
-vim.api.nvim_set_keymap('n', '[t', [[<Plug>(ultest-prev-fail)]], { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>tt', [[<Plug>(ultest-run-file)]], { silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>tn', [[<Plug>(ultest-run-nearest)]], { silent = true })
+map('n', ']t', [[<Plug>(ultest-next-fail)]], opts)
+map('n', '[t', [[<Plug>(ultest-prev-fail)]], opts)
+map('n', '<Leader>tt', [[<Plug>(ultest-run-file)]], opts)
+map('n', '<Leader>tn', [[<Plug>(ultest-run-nearest)]], opts)
