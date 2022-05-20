58272dd1670a3d08669cd8e5af3ab06e394e798a
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2022-05-20 10:40:04 +0100

    Changed powershell startup for komorebi into dotfiles instead

--- windows/scripts/cmd/startup.cmd
+++ windows/scripts/cmd/startup.cmd
@@ -1,3 +1,3 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted CurrentUser" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell %USERPROFILE%\.config\scripts\powershell\start-komorebi.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell %USERPROFILE%\dotfiles-work\windows\scripts\powershell\start-komorebi.ps1 >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell -Command “Set-ExecutionPolicy Restricted” >> "%TEMP%\StartupLog.txt" 2>&1
