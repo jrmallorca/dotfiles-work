df83cdc9d73c1963e45871665a991389745d6baa
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2022-07-08 16:23:18 +0100

    Added new configuration for autopairs

--- unix/config/nvim/lua/pkg-autopairs.lua
+++ unix/config/nvim/lua/pkg-autopairs.lua
@@ -1,43 +1,13 @@
-local npairs = require("nvim-autopairs")
-local Rule = require('nvim-autopairs.rule')
-
-npairs.setup({
-    enable_check_bracket_line = false, -- If next character is a close pairs and it doesn't have an open pairs in
-                                       -- same line then don't add a close pairs
-
-    fast_wrap = {
-      map = '<M-e>',
-      chars = { '{', '[', '(', '"', "'" },
-      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
-      end_key = '$',
-      keys = 'qwertyuiopzxcvbnmasdfghjkl',
-      check_comma = true,
-      hightlight = 'Search'
-    },
-
-    check_ts = true,
-    ts_config = {
-        lua = {'string'},-- it will not add pair on that treesitter node
-        javascript = {'template_string'},
-        java = false,-- don't check treesitter on java
-    }
+require('nvim-autopairs').setup({
+  ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
+  enable_check_bracket_line = false,
+  fast_wrap = {}
 })
 
-require('nvim-treesitter.configs').setup {
-    autopairs = {enable = true}
-}
-
-local ts_conds = require('nvim-autopairs.ts-conds')
-
--- press % => %% is only inside comment or string
-npairs.add_rules({
-  Rule("%", "%", "lua")
-    :with_pair(ts_conds.is_ts_node({'string','comment'})),
-  Rule("$", "$", "lua")
-    :with_pair(ts_conds.is_not_ts_node({'function'}))
-})
-
-require("nvim-autopairs.completion.compe").setup({
-  map_cr = true,       -- Map <CR> on insert mode
-  map_complete = true, -- Auto insert `(` after select function or method item
-})
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
